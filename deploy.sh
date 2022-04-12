2155ba88db2b9e8a68850c4849571ab134522279
unknown <loganlaughery@gmail.com>
2022-04-12 15:00:57 -0700

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ~/.nvm/nvm.sh;
 
 # View deployment logs at journalctl -S today -f -u githubDeployer.service
 /home/logan/.nvm/versions/node/v17.9.0/bin/npm ci --unsafe-perm=true --allow-root
