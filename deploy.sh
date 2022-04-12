c5bcdb3017cac89bebd1cfe82a11d1e500525b0c
unknown <loganlaughery@gmail.com>
2022-04-12 14:19:27 -0700

    Try different source for npm

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 
 # View deployment logs at journalctl -S today -f -u githubDeployer.service
-npm ci --unsafe-perm=true --allow-root
+/home/logan/.nvm/versions/node/v17.9.0/bin/npm ci --unsafe-perm=true --allow-root
 cp sbrDocumentApi.service /etc/systemd/system/sbrDocumentApi.service
 systemctl restart sbrDocumentApi
 
