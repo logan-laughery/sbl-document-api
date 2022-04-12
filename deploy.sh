add6101902f5b02a5b2cabb5c7bd8bf8bc4674c3
unknown <loganlaughery@gmail.com>
2022-04-12 11:42:23 -0700

    Add service for starting node process

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+
+# View deployment logs at journalctl -S today -f -u githubDeployer.service
 npm ci --unsafe-perm=true --allow-root
+cp sbrDocumentApi.service /etc/systemd/system/sbrDocumentApi.service
+systemctl restart sbrDocumentApi
+
+echo $(systemctl status sbrDocumentApi)
