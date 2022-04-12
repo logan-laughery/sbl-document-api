d78c3568c9cf4884bee0ac3086e06b21e4ffb16d
unknown <loganlaughery@gmail.com>
2022-04-12 14:25:20 -0700

    Change service user

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 # View deployment logs at journalctl -S today -f -u githubDeployer.service
 /home/logan/.nvm/versions/node/v17.9.0/bin/npm ci --unsafe-perm=true --allow-root
 cp sbrDocumentApi.service /etc/systemd/system/sbrDocumentApi.service
-systemctl restart sbrDocumentApi
+systemctl stop sbrDocumentApi
+systemctl daemon-reload
+systemctl enable sbrDocumentApi
+systemctl start sbrDocumentApi
 
 echo $(systemctl status sbrDocumentApi)
