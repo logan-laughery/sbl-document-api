5d3442cde788d3fdbbb7d47b6b00b1dddebdd304
unknown <loganlaughery@gmail.com>
2022-04-13 13:59:49 -0700

    Restrict which actions are exposed

--- src/createSchema.ts
+++ src/createSchema.ts
@@ -1,10 +1,20 @@
 import { buildSchema } from "type-graphql";
 import * as path from "path";
-import { resolvers } from "../prisma/generated/type-graphql";
+// import { resolvers } from "../prisma/generated/type-graphql";
+import {
+  FindUniqueDocumentResolver,
+  FindManyDocumentResolver,
+  relationResolvers
+} from "../prisma/generated/type-graphql";
 import {resolvers as documentResolvers} from "./resolvers/Documents";
 
 export const createSchema = async () =>
   await buildSchema({
-    resolvers: [...resolvers, ...documentResolvers],
+    resolvers: [
+      FindUniqueDocumentResolver,
+      FindManyDocumentResolver,
+      ...relationResolvers,
+      ...documentResolvers
+    ],
     // emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql")
   });
