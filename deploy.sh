daf056de01af37cd8474403d834685f41f6dbaed
logan-laughery <logan.laughery@gmail.com>
2022-04-12 20:51:17 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ source /home/logan/.nvm/nvm.sh;
 
 # View deployment logs at journalctl -S today -f -u githubDeployer.service
 /home/logan/.nvm/versions/node/v17.9.0/bin/npm ci --unsafe-perm=true --allow-root
+chmod +x ./start.sh
 cp sbrDocumentApi.service /etc/systemd/system/sbrDocumentApi.service
 systemctl stop sbrDocumentApi
 systemctl daemon-reload
